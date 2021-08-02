d71e4393b86307570f494346c15370d58d1ab2c4
totherain155 <totherain155@gmail.com>
2021-08-02 15:23:45 +0900

    practice reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
-import Lang from "./Context";
-import Screen from "./Screen";
-import translations from "./translation";
+import React, { useReducer } from "react";
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  } // return할 object는 state를 대체하게 될 object이다.
+  // 이건 state를 변경하는 게 아니고 override 하는 것도 아니다 .
+  // 추가하는 것도 아닌 state를 완전히 '대체'하는 것이다.
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // dispatch가 하는 일은 reducer에 action을 보내 주는 것이다.
   return (
-    <Lang defaultLang="en" translations={translations}>
-      <Screen />
-    </Lang>
+    <>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>Minus</button>
+    </>
   );
 };
 
